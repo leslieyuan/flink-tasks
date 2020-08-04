dbc6020998f6a95b0c2347593707020a4d8c9605
yuanlong <yuanlong@cestc.cn>
2020-08-04 09:14:05 +0800

    add example for beijing env

--- src/main/java/com/ly/sql/DoInBeijing.java
+++ src/main/java/com/ly/sql/DoInBeijing.java
@@ -16,17 +16,17 @@ public class DoInBeijing {
 
         tableEnv.sqlUpdate("create\n" +
                 "\ttable\n" +
-                "\t\twork1(\n" +
+                "\t\ttest1(\n" +
                 "\t\t\tFWSQRYMC VARCHAR\n" +
                 "\t\t) with(\n" +
                 "\t\t\t'connector.type' = 'kafka',\n" +
                 "\t\t\t'connector.version' = 'universal',\n" +
-                "\t\t\t'connector.topic' = 'work2',\n" +
+                "\t\t\t'connector.topic' = 'test1',\n" +
                 "\t\t\t'connector.startup-mode' = 'latest-offset',\n" +
                 "\t\t\t'connector.properties.0.key' = 'zookeeper.connect',\n" +
-                "\t\t\t'connector.properties.0.value' = '172.17.78.247:2181',\n" +
+                "\t\t\t'connector.properties.0.value' = '172.16.87.101:2181,172.16.87.102:2181,172.16.87.103:2181',\n" +
                 "\t\t\t'connector.properties.1.key' = 'bootstrap.servers',\n" +
-                "\t\t\t'connector.properties.1.value' = '172.17.78.247:6667',\n" +
+                "\t\t\t'connector.properties.1.value' = '172.16.87.101:6667',\n" +
                 "\t\t\t'connector.properties.2.key' = 'group.id',\n" +
                 "\t\t\t'connector.properties.2.value' = '02020062216021101400000101001103',\n" +
                 "\t\t\t'format.type' = 'json',\n" +
@@ -35,23 +35,29 @@ public class DoInBeijing {
 
         tableEnv.sqlUpdate("create\n" +
                 "\ttable\n" +
-                "\t\tschool(\n" +
-                "\t\t\tid varchar\n" +
+                "\t\ttest2(\n" +
+                "\t\t\theheda VARCHAR\n" +
                 "\t\t) with(\n" +
-                "\t\t\t'connector.type' = 'jdbc',\n" +
-                "\t\t\t'connector.url' = 'jdbc:mysql://172.17.78.250:3306/binlog_test',\n" +
-                "\t\t\t'connector.table' = 'school',\n" +
-                "\t\t\t'connector.username' = 'remote',\n" +
-                "\t\t\t'connector.password' = 'Cestc.02',\n" +
-                "\t\t\t'connector.write.flush.max-rows' = '1'\n" +
+                "\t\t\t'connector.type' = 'kafka',\n" +
+                "\t\t\t'connector.version' = 'universal',\n" +
+                "\t\t\t'connector.topic' = 'test2',\n" +
+                "\t\t\t'connector.startup-mode' = 'latest-offset',\n" +
+                "\t\t\t'connector.properties.0.key' = 'zookeeper.connect',\n" +
+                "\t\t\t'connector.properties.0.value' = '172.16.87.101:2181,172.16.87.102:2181,172.16.87.103:2181',\n" +
+                "\t\t\t'connector.properties.1.key' = 'bootstrap.servers',\n" +
+                "\t\t\t'connector.properties.1.value' = '172.16.87.101:6667',\n" +
+                "\t\t\t'connector.properties.2.key' = 'group.id',\n" +
+                "\t\t\t'connector.properties.2.value' = '02020062216021101400000101001103',\n" +
+                "\t\t\t'format.type' = 'json',\n" +
+                "\t\t\t'format.derive-schema' = 'true'\n" +
                 "\t\t)");
 
         tableEnv.sqlUpdate("insert\n" +
                 "\tinto\n" +
-                "\t\tschool(IDENTITY) select\n" +
+                "\t\ttest2(heheda) select\n" +
                 "\t\t\tFWSQRYMC\n" +
                 "\t\tfrom\n" +
-                "\t\t\twork1");
+                "\t\t\ttest1");
 
         tableEnv.execute("test");
     }
