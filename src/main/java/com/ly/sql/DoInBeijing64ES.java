fa215fc0e23633d1f3a8e32981c8e281c5a59e10
yuanlong <yuanlong@cestc.cn>
2020-08-10 17:14:00 +0800

    add a beijing 64 env demo

--- src/main/java/com/ly/sql/DoInBeijing64ES.java
+++ src/main/java/com/ly/sql/DoInBeijing64ES.java
@@ -6,7 +6,7 @@ import org.apache.flink.table.api.java.StreamTableEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DoInBeijingES {
+public class DoInBeijing64ES {
     private static final Logger log = LoggerFactory.getLogger(ParseNestedJsonWin.class);
 
     public static void main(String[] args) throws Exception {
@@ -22,10 +22,11 @@ public class DoInBeijingES {
                 ") WITH (\n" +
                 "  'connector.type' = 'kafka',\n" +
                 "  'connector.version' = 'universal',\n" +
-                "  'connector.topic' = 'stuTable-test',\n" +
+                "  'connector.topic' = 'yuanlongtest1',\n" +
                 "  'connector.startup-mode' = 'latest-offset',\n" +
                 "  'connector.properties.zookeeper.connect' = 'hdp1.ambari:2181',\n" +
                 "  'connector.properties.bootstrap.servers' = 'hdp1.ambari:6667',\n" +
+                "  'connector.properties.group.id' = 'consumer1',\n" +
                 "  'format.type' = 'json'\n" +
                 ")");
 
@@ -44,7 +45,7 @@ public class DoInBeijingES {
                 "  'connector.flush-on-checkpoint' = 'false', \n" +
                 "  'connector.bulk-flush.max-actions' = '1',\n" +
                 "  'connector.bulk-flush.max-size' = '1 mb',\n" +
-                "  'connector.bulk-flush.interval' = '1000', \n" +
+                "  'connector.bulk-flush.interval' = '1', \n" +
                 "  'connector.bulk-flush.backoff.max-retries' = '3',\n" +
                 "  'connector.bulk-flush.backoff.delay' = '1000',\n" +
                 "  'format.type' = 'json'\n" +
