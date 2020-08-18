bf71cbe94f16040254ae72c15a180bb1ead8684c
yuanlong <yuanlong@cestc.cn>
2020-08-18 18:24:11 +0800

    first init

--- src/main/java/com/ly/sql/DoInWuhan.java
+++ src/main/java/com/ly/sql/DoInWuhan.java
@@ -1,74 +1,102 @@
 package com.ly.sql;
 
-import org.apache.flink.api.common.restartstrategy.RestartStrategies;
-import org.apache.flink.api.common.time.Time;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
-import org.apache.flink.table.api.java.StreamTableEnvironment;
-
-import java.util.concurrent.TimeUnit;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 public class DoInWuhan {
     public static void main(String[] args) throws Exception {
         // 环境变量
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        EnvironmentSettings fsSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build();
-        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env, fsSettings);
-        env.enableCheckpointing(30000);
-        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(5, Time.of(10, TimeUnit.SECONDS)));
+        StreamExecutionEnvironment bsEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+        EnvironmentSettings bsSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build();
+        StreamTableEnvironment bsTableEnv = StreamTableEnvironment.create(bsEnv, bsSettings);
 
         String jobName = args[0];
 
         // source file
-        tableEnv.sqlUpdate("create table work1(\n" +
-                "  SJSWJG_DM STRING,\n" +
-                "  SLRYS STRING,\n" +
-                "  SWJG_DM STRING,\n" +
-                "  SLYWS STRING\n" +
-                ") with (\n" +
-                "  'connector.type' = 'kafka',\n" +
-                "  'connector.version' = 'universal',\n" +
-                "  'connector.topic' = 'yuanlong',\n" +
-                "  'connector.startup-mode' = 'earliest-offset',\n" +
-                "  'connector.properties.0.key' = 'zookeeper.connect',\n" +
-                "  'connector.properties.0.value' = '10.101.236.2:2181',\n" +
-                "  'connector.properties.1.key' = 'bootstrap.servers',\n" +
-                "  'connector.properties.1.value' = '10.101.236.2:6667',\n" +
-                "  'connector.properties.2.key' = 'group.id',\n" +
-                "  'connector.properties.2.value' = '02020080411124458000000101001100',\n" +
-                "  'format.type' = 'json',\n" +
-                "  'format.derive-schema' = 'true'\n" +
+        bsTableEnv.executeSql("CREATE TABLE t_nftywll(\n" +
+                "  v_operation VARCHAR,\n" +
+                "  v_timestamp VARCHAR,\n" +
+                "  v_table_name VARCHAR,\n" +
+                "  v_database VARCHAR,\n" +
+                "  SJSWJG_DM VARCHAR,\n" +
+                "  SJSWJGMC VARCHAR,\n" +
+                "  SWJG_DM VARCHAR,\n" +
+                "  SWJGMC VARCHAR,\n" +
+                "  FWQDLX_DM VARCHAR,\n" +
+                "  YWLSID VARCHAR,\n" +
+                "  YWSLRYMC VARCHAR,\n" +
+                "  FWSQRYMC VARCHAR,\n" +
+                "  YWLX_DM VARCHAR,\n" +
+                "  KSPDSJ BIGINT,\n" +
+                "  KSBLYWSJ BIGINT,\n" +
+                "  JSBLYWSJ BIGINT\n" +
+                ") WITH (\n" +
+                " 'connector' = 'kafka',\n" +
+                " 'topic' = 'yuanlongtest',\n" +
+                " 'properties.bootstrap.servers' = 'hdp1.ambari:6667',\n" +
+                " 'properties.group.id' = 'testGroup',\n" +
+                " 'format' = 'json',\n" +
+                " 'scan.startup.mode' = 'earliest-offset',\n" +
+                " 'sink.partitioner' = 'round-robin'\n" +
                 ")");
 
-        // destination file
-        tableEnv.sqlUpdate("create table DM_GY_SWJG_LJS(SWJG_DM STRING, SWJGMC STRING, SWJGJC STRING) with (\n" +
-                "  'connector.type' = 'jdbc',\n" +
-                "  'connector.url' = 'jdbc:mysql://10.101.236.3:3306/binlog_test?useUnicode=true&characterEncoding=UTF-8',\n" +
-                "  'connector.table' = 'DM_GY_SWJG_LJS',\n" +
-                "  'connector.username' = 'remote',\n" +
-                "  'connector.password' = 'C1stc.0e',\n" +
-                "  'connector.write.flush.max-rows' = '1'\n" +
+        bsTableEnv.executeSql("CREATE TABLE T_YY_ZHDP_SWDJZTJK_BSTHXZL_LJS_yuanlong(\n" +
+                "  SJSWJG_DM VARCHAR,\n" +
+                "  SJSWJGMC VARCHAR,\n" +
+                "  SWJG_DM VARCHAR,\n" +
+                "  SWJGMC VARCHAR,\n" +
+                "  KFCKS DOUBLE,\n" +
+                "  KFZDS DOUBLE,\n" +
+                "  SLYWS DOUBLE,\n" +
+                "  SLRYS DOUBLE,\n" +
+                "  PJDDHS DOUBLE,\n" +
+                "  PJBLHS DOUBLE,\n" +
+                "  PRIMARY KEY (SJSWJG_DM, SJSWJGMC, SWJG_DM, SWJGMC) NOT ENFORCED\n" +
+                ") WITH (\n" +
+                "  'connector' = 'jdbc',\n" +
+                "  'url' = 'jdbc:mysql://10.101.236.3:3306/binlog_test?useUnicode=true&characterEncoding=UTF-8',\n" +
+                "  'table-name' = 'T_YY_ZHDP_SWDJZTJK_BSTHXZL_LJS_yuanlong',\n" +
+                "  'username' = 'remote',\n" +
+                "  'password' = 'C1stc.0e',\n" +
+                "  'lookup.max-retries' = '3',\n" +
+                "  'sink.buffer-flush.max-rows' = '500',\n" +
+                "  'sink.buffer-flush.interval' = '2s',\n" +
+                "  'sink.max-retries' = '3'\n" +
                 ")");
 
-        // doing
-        tableEnv.sqlUpdate("insert into\n" +
-                "  DM_GY_SWJG_LJS(SWJG_DM, SWJGMC, SWJGJC)\n" +
-                "select\n" +
-                "  SWJG_DM,\n" +
+        bsTableEnv.executeSql("INSERT INTO\n" +
+                "  T_YY_ZHDP_SWDJZTJK_BSTHXZL_LJS_yuanlong\n" +
+                "SELECT\n" +
                 "  SJSWJG_DM,\n" +
-                "  SLYWS\n" +
-                "from\n" +
-                "  (\n" +
-                "    SELECT\n" +
-                "      UPPER(SJSWJG_DM) AS SJSWJG_DM,\n" +
-                "      SLRYS,\n" +
-                "      SWJG_DM,\n" +
-                "      SLYWS\n" +
-                "    FROM\n" +
-                "      work1\n" +
-                "  )");
+                "  SJSWJGMC,\n" +
+                "  SWJG_DM,\n" +
+                "  SWJGMC,\n" +
+                "  SUM(\n" +
+                "    CASE\n" +
+                "      when FWQDLX_DM = '01' then 1\n" +
+                "      else 0\n" +
+                "    end\n" +
+                "  ) KFCKS,\n" +
+                "  SUM(\n" +
+                "    CASE\n" +
+                "      when FWQDLX_DM = '02' then 1\n" +
+                "      else 0\n" +
+                "    end\n" +
+                "  ) KFZDS,\n" +
+                "  COUNT(1) SLYWS,\n" +
+                "  COUNT(DISTINCT FWSQRYMC) SLRYS,\n" +
+                "  ROUND(SUM((KSBLYWSJ - KSPDSJ) * 24 * 60), 2) PJDDHS,\n" +
+                "  ROUND(SUM((JSBLYWSJ - KSBLYWSJ) * 24 * 60), 2) PJBLHS\n" +
+                "FROM\n" +
+                "  t_nftywll\n" +
+                "GROUP BY\n" +
+                "  SJSWJG_DM,\n" +
+                "  SJSWJGMC,\n" +
+                "  SWJG_DM,\n" +
+                "  SWJGMC");
 
         // start
-        tableEnv.execute(jobName);
+        bsTableEnv.execute(jobName);
     }
 }
