f731d0ffff939a75c8f60d8fb2a44ca68cb28d86
yuanlong <yuanlong1990@gmail.com>
2020-12-22 13:38:34 +0800

    add useful configurations

--- src/main/java/com/ly/Hbase2EsApp.java
+++ src/main/java/com/ly/Hbase2EsApp.java
@@ -1,14 +1,28 @@
 package com.ly;
 
 import com.ly.util.ReadStringFromFile;
+import com.ly.util.TransUtil;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.PipelineOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
+import java.util.Properties;
+
 public class Hbase2EsApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // 环境变量
-        StreamExecutionEnvironment bsEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+        Properties properties = new Properties();
+        properties.load(Hbase2EsApp.class.getClassLoader().getResourceAsStream("global.properties"));
+
+        Configuration configuration = new Configuration();
+        configuration.set(PipelineOptions.NAME, "my_task_name");
+        configuration.set(PipelineOptions.GLOBAL_JOB_PARAMETERS, TransUtil.pro2Map(properties));
+        configuration.set(PipelineOptions.MAX_PARALLELISM, 10);
+        configuration.set(PipelineOptions.OPERATOR_CHAINING, true);
+
+        StreamExecutionEnvironment bsEnv = new StreamExecutionEnvironment(configuration);
         EnvironmentSettings bsSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build();
         StreamTableEnvironment bsTableEnv = StreamTableEnvironment.create(bsEnv, bsSettings);
 
